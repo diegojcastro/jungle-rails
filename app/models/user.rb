d98b674679ba2cc7055d40e6629b2227a012b4fd
Diego Castro <diegojcr4@gmail.com>
2021-10-18 19:28:52 -0400

    add password size and email uniqueness validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 4 }
+
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,27 +10,31 @@ RSpec.describe User, type: :model do
     it 'should not create a valid record without a password' do
       @user = User.new(
         name: 'Diego',
+        last_name: 'Castro',
         email: 'diego@diego.com'
       )
       @user.save
       expect(@user.id).to be_nil
     end
 
-    it 'should create a valid user after saving given a password digest' do
-      @user = User.new(
-        name: 'Diego',
-        email: 'diego@diego.com',
-        password_digest: 'abcdefghijklmnop'
-      )
-      @user.save
-      expect(@user.id).to be_present
-      expect(@user.name).to eq('Diego')
-      expect(@user.email).to eq('diego@diego.com')
-    end
+    # This was true but shouldn't be. But I think I shouldn't touch the digest directly.
+    # it 'should create a valid user after saving given a password digest' do
+    #   @user = User.new(
+    #     name: 'Diego',
+    #     last_name: 'Castro',
+    #     email: 'diego@diego.com',
+    #     password_digest: 'abcdefghijklmnop'
+    #   )
+    #   @user.save
+    #   expect(@user.id).to be_present
+    #   expect(@user.name).to eq('Diego')
+    #   expect(@user.email).to eq('diego@diego.com')
+    # end
 
     it 'should create a valid user after saving given a password and password confirmation that match' do
       @user = User.new(
         name: 'Diego',
+        last_name: 'Castro',
         email: 'diego@diego.com',
         password: 'hello12345goodbye',
         password_confirmation: 'hello12345goodbye'
@@ -44,6 +48,7 @@ RSpec.describe User, type: :model do
     it 'should not create a user after saving given a password and password confirmation that do not match' do
       @user = User.new(
         name: 'Diego',
+        last_name: 'Castro',
         email: 'diego@diego.com',
         password: 'hello12345goodbye',
         password_confirmation: 'goodbye12345hello'
@@ -55,6 +60,7 @@ RSpec.describe User, type: :model do
     it 'should not create a user if the email is already taken' do
       @user = User.new(
         name: 'Diego',
+        last_name: 'Castro',
         email: 'diego@diego.com',
         password: 'hello12345goodbye',
         password_confirmation: 'hello12345goodbye'
@@ -66,14 +72,87 @@ RSpec.describe User, type: :model do
 
       @user2 = User.new(
         name: 'Billybob',
+        last_name: 'Castro',
         email: 'diego@diego.com',
         password: 'another123password',
         password_confirmation: 'another123password'
       )
       @user2.save
-      expect(@user.id).to be_nil
-
+      expect(@user2.id).to be_nil
     end
+
+    it 'should not create a user without a name' do
+      @user = User.new(
+        name: nil,
+        last_name: 'Castro',
+        email: 'diego@diego3.com',
+        password: 'hello12345goodbye',
+        password_confirmation: 'goodbye12345hello'
+      )
+      @user.save
+      expect(@user.id).to be_nil
+    end
+
+    it 'should not create a user without a last name' do
+      @user = User.new(
+        name: 'Diego',
+        last_name: nil,
+        email: 'diego@diego3.com',
+        password: 'hello12345goodbye',
+        password_confirmation: 'goodbye12345hello'
+      )
+      @user.save
+      expect(@user.id).to be_nil
+    end
+
+    it 'should not create a user without an email' do
+      @user = User.new(
+        name: 'Diego',
+        last_name: 'Castro',
+        email: nil,
+        password: 'hello12345goodbye',
+        password_confirmation: 'goodbye12345hello'
+      )
+      @user.save
+      expect(@user.id).to be_nil
+    end
+
+    it 'should not create a user if the email is already taken with different capitalization' do
+      @user = User.new(
+        name: 'Diego',
+        last_name: 'Castro',
+        email: 'DIEGO4@diego.com',
+        password: 'hello12345goodbye',
+        password_confirmation: 'hello12345goodbye'
+      )
+      @user.save
+      expect(@user.id).to be_present
+      expect(@user.name).to eq('Diego')
+      expect(@user.email).to eq('DIEGO4@diego.com')
+
+      @user2 = User.new(
+        name: 'Billybob',
+        last_name: 'Castro',
+        email: 'diego4@diego.com',
+        password: 'another123password',
+        password_confirmation: 'another123password'
+      )
+      @user2.save
+      expect(@user2.id).to be_nil
+    end
+
+    it 'should not create a user with a password under length 4' do
+      @user = User.new(
+        name: 'Diego',
+        last_name: 'Castro',
+        email: 'diego3@diego3.com',
+        password: 'hi',
+        password_confirmation: 'hi'
+      )
+      @user.save
+      expect(@user.id).to be_nil
+    end
+
 
   end
 
