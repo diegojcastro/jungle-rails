d063a20e5cf74b98059e144c539c2087b0dcdb27
Diego Castro <diegojcr4@gmail.com>
2021-10-19 13:36:09 -0400

    add home page testing functions

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(word_count: 3),
+        description: Faker::Hipster.paragraph(sentence_count: 4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product', count: 10
+  end
+end
